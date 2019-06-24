cf9f51c13f6b6c4b27f091474cd8d5341880d3bd
dana taylor <48800966+overfl0wd@users.noreply.github.com>
2019-06-24 11:39:32 -0400

    adding filename var

--- secretScan.sh
+++ secretScan.sh
@@ -3,8 +3,8 @@
 if [ $# -lt 2 ]
 then
 	echo -e "\e[91mNot enough arguments supplied"
-	echo -e "\e[93mUsage: secretScan.sh <target directory> <output directory>"
-	echo "Example: secretScan.sh phoenix/ phoenix/secrets/"
+	echo -e "\e[93mUsage: $0 <target directory> <output directory>"
+	echo "Example: $0 phoenix/ phoenix/secrets/"
 	exit 1
 fi
 
